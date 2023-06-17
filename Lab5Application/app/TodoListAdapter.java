d631214203e44d2a92a546c921ba202f38c57198
lnguyen7003 <lnguyen7003@gmail.com>
2023-06-16 21:15:33 -0400

    Lab 5 Initial Commit

---
+++ Lab5Application/app/TodoListAdapter.java
@@ -0,0 +1,57 @@
+import android.content.Context;
+import android.graphics.Color;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class TodoListAdapter extends BaseAdapter {
+    private Context context;
+    private List<MainActivity.TodoItem> itemList;
+
+    public TodoListAdapter(Context context, List<MainActivity.TodoItem> itemList) {
+        this.context = context;
+        this.itemList = itemList;
+    }
+
+    @Override
+    public int getCount() {
+        return itemList.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return itemList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            LayoutInflater inflater = LayoutInflater.from(context);
+            convertView = inflater.inflate(android.R.layout.simple_list_item_1, parent, false);
+        }
+
+        TextView textView = convertView.findViewById(android.R.id.text1);
+        MainActivity.TodoItem item = itemList.get(position);
+        textView.setText(item.getText());
+        textView.setTextSize(20);
+
+        if (item.isUrgent()) {
+            convertView.setBackgroundColor(Color.RED);
+            textView.setTextColor(Color.WHITE);
+        } else {
+            convertView.setBackgroundColor(Color.WHITE);
+            textView.setTextColor(Color.BLACK);
+        }
+
+        return convertView;
+    }
+}
